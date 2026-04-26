a737b83d06d70543e6a4e7242d72dabdf425ca08
NullPointer274 <lik2306@yandex.ru>
2026-04-26 23:10:18 +1000

    Добавлен запуск бота

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Привет!")
+
+if __name__ == '__main__':
+    print("Бот запущен...")
+    bot.infinity_polling()
